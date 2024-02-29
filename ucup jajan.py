3ad1febcef37d770ce63949b6e752d049777600c
rulifarel <160815275+rulifarel@users.noreply.github.com>
2024-02-29 11:27:25 +0700

    Add files via upload

---
+++ ucup jajan.py
@@ -0,0 +1,13 @@
+
+rokok = 20000
+minyak = 25000
+gula =  11000
+saldo = 100000
+diskon = 10000
+kembalian = saldo - ((rokok + minyak + gula) - diskon)
+print("print : ", rokok)
+print("print : ", minyak)
+print("print : ", gula)
+print("print : ",diskon)
+print("print : ",saldo)
+print("kembalian ucup :",kembalian)
